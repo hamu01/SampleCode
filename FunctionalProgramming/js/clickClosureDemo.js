af8a3fc7dea5e29a5ddeda2ca84edbd92b1b3e9b
Mayanlong <mayanlong@corp.beisencorp.com>
2019-07-08 18:56:06 +0800

    1. 清理无用代码

--- FunctionalProgramming/js/clickClosureDemo.js
+++
@@ -1,9 +0,0 @@
-var add_the_handlers = function (nodes) {
-var i;
-for (i = 0; i < nodes.length; i += 1) {
-	nodes[i].onclick = (function (index) {
-		return function(e){							
-			alert(index);
-		}
-	})(i)
-}
