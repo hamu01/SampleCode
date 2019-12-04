a922c25b55bee6dffb64a18c016e4ec402bc140a
Mayanlong <mayanlong@corp.beisencorp.com>
2019-12-04 19:19:20 +0800

    1. 添加not equal相关的Binary Search算法

--- Algorithms/BinarySearch/Program.cs
+++ Algorithms/BinarySearch/Program.cs
@@ -1,37 +1,91 @@
 ﻿using System;
+using System.Linq;
 
 namespace BinarySearch
 {
     class Program
     {
         private static Random random = new Random(DateTime.Now.Millisecond);
+
         static void Main(string[] args)
         {
-            Search search = new Search();
-            int[] numbers = GetNumbers(10);
-            int target = numbers[random.Next(0, numbers.Length - 1)];
-            int index = search.SearchEqual(numbers, target);
-            Print(numbers, target, index);
-
-            numbers = GetNumbers(10);
-            target = numbers[random.Next(0, numbers.Length - 1)];
-            index = search.SearchEqual(numbers, target);
-            Print(numbers, target, index);
+            var search = new Search();
+            var numberTypes = new string[] { "random", "default" };
+            var targetTypes = new string[] { "first", "last", "outofrange", "default" };
+            var searchFuncs = new Func<int[], int, int>[] {
+                search.SearchEqual, search.SearchEqual1, 
+                search.SearchNotLessThan, search.SearchLessThan, 
+                search.SearchNotGreaterThan, search.SearchGreaterThan
+            };
+            foreach (var numberType in numberTypes)
+            {
+                Console.WriteLine($"number type: {numberType}");
+                int[] numbers = GetNumbers(10, numberType);
+                foreach (var searchFunc in searchFuncs)
+                {
+                    Console.WriteLine(searchFunc.Method.Name);
+                    foreach (var targetType in targetTypes)
+                    {
+                        int target = GetTarget(numbers, targetType);
+                        int index = searchFunc(numbers, target);
+                        Print(numbers, targetType, target, index);
+                    }
+                }
+                Console.WriteLine();
+            }
         }
 
-        private static void Print(int[] numbers, int target, int index)
+        private static void Print(int[] numbers, string targetType, int target, int index)
         {
             string s = string.Join(",", numbers);
-            Console.WriteLine($"{target} in [{s}] is {index}");
+            Console.WriteLine($"{target}[{targetType}] in [{s}] is {index}");
         }
 
-        private static int[] GetNumbers(int n)
+        private static int GetTarget(int[] numbers, string type = "default")
+        {
+            int target;
+            switch (type)
+            {
+                case "first":
+                    target = numbers.First();
+                    break;
+
+                case "last":
+                    target = numbers.Last();
+                    break;
+
+                case "outofrange":
+                    target = numbers.First() - 1;
+                    break;
+
+                default:
+                    target = numbers[random.Next(0, numbers.Length - 1)];
+                    break;
+            }
+            return target;
+        }
+
+        private static int[] GetNumbers(int n, string type = "default")
         {
             int[] numbers = new int[n];
-            for (int i = 0; i < n; i++)
+            switch (type)
             {
-                numbers[i] = random.Next(10, 1000);
+                case "random":
+                    for (int i = 0; i < n; i++)
+                    {
+                        numbers[i] = random.Next(10, 1000);
+                    }
+                    break;
+
+                default:
+                    int num = random.Next(10, 1000);
+                    for (int i = 0; i < n; i++)
+                    {
+                        numbers[i] = num;
+                    }
+                    break;
             }
+
             Array.Sort(numbers);
             return numbers;
         }
--- Algorithms/BinarySearch/Search.cs
+++ Algorithms/BinarySearch/Search.cs
@@ -29,5 +29,82 @@ namespace BinarySearch
             }
             return -1;
         }
+
+        // >=, 退出循环后一定满足lo=hi+1
+        public int SearchNotLessThan(int[] numbers, int target)
+        {
+            int lo = 0, hi = numbers.Length - 1;
+            while (lo <= hi)
+            {
+                int mid = (hi - lo) / 2 + lo;
+                if (target <= numbers[mid])
+                {
+                    hi = mid - 1;
+                }
+                else
+                {
+                    lo = mid + 1;
+                }
+            }
+            return lo; // or hi+1
+        }
+
+        // <
+        public int SearchLessThan(int[] numbers, int target)
+        {
+            int lo = 0, hi = numbers.Length - 1;
+            while (lo <= hi)
+            {
+                int mid = (hi - lo) / 2 + lo;
+                if (target <= numbers[mid])
+                {
+                    hi = mid - 1;
+                }
+                else
+                {
+                    lo = mid + 1;
+                }
+            }
+            return hi; // or lo-1
+        }
+
+        // <=
+        public int SearchNotGreaterThan(int[] numbers, int target)
+        {
+            int lo = 0, hi = numbers.Length - 1;
+            while (lo <= hi)
+            {
+                int mid = (hi - lo) / 2 + lo;
+                if (target >= numbers[mid])
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+            return hi; // or lo-1
+        }
+
+        // >
+        public int SearchGreaterThan(int[] numbers, int target)
+        {
+            int lo = 0, hi = numbers.Length - 1;
+            while (lo <= hi)
+            {
+                int mid = hi - lo / 2 + lo;
+                if (target >= numbers[mid])
+                {
+                    lo = mid + 1;
+                    
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+            return lo; // or hi+1
+        }
     }
 }
